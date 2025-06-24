a67ee0ec0edfad05196fc273e53e8ef3d1f39032
Leo <89448004+Le0l0@users.noreply.github.com>
2025-06-24 11:07:28 +0200

    Add files via upload

---
+++ part_2/online_banka/src/main/java/com/bank_server/online_banka/OnlineBankAppServer.java
@@ -0,0 +1,118 @@
+package com.bank_server.online_banka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+////////////////////////////////////////////////////////////////
+import java.util.ArrayList;
+import java.time.LocalDate;
+
+
+// tipovi poruka - objekti za komunikaciju (DTOs)
+record UserM(String username, String password, char encryption) {}
+record BankAccM(String IBAN, double balance, String value) {}
+record TransactionReq(String recipient, double amount) {}
+record TransactionM(String payer, String recipient, double amount, LocalDate date) {}
+
+
+
+
+@SpringBootApplication
+@RestController
+public class OnlineBankAppServer
+{
+	// stanje servera - nigdje se ne mijenja
+	private static String state = "hello";
+	
+	
+	
+	// pokreni server
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankAppServer.class, args);
+	}
+	
+	
+	
+	// vrati stanje servera - za sada se koristi samo za provjeru je li server pokrenut
+	@GetMapping("/")
+	public String serverState() {
+		return state;
+	}
+	
+	
+	
+	// vraca odgovor postoji li korisnik s unesenim imenom
+	@GetMapping("/users/{username}/exists")
+	public boolean checkUserExists(@PathVariable String username) {
+		return User.userExists(username);
+	}
+	
+	
+	
+	// vraca objekt tipa BankAccM koji sadrzi informacije o korisnikovom racunu
+	@GetMapping("/users/{username}/account-details")
+	public BankAccM getAccDetails(@PathVariable String username) {
+		BankAccount acc = BankAccount.getAccount(username);
+		return new BankAccM(acc.IBAN, acc.balance, acc.value);
+	}
+	
+	// vraca (nesortiranu) listu transakcija
+	@GetMapping("/users/{username}/history")
+	public ResponseEntity<ArrayList<TransactionM>> getAccHistory(@PathVariable String username) {
+		return new ResponseEntity<ArrayList<TransactionM>>(Transaction.loadTransactionList(username), HttpStatus.OK);
+	}
+	
+	
+	
+	// registracija korisnika
+	@PostMapping("/users/registration")
+	public boolean registerUser(@RequestBody UserM userM) {
+		return User.registration(userM.username(), userM.password(), userM.encryption());
+	}
+	
+	// prijava korisnika
+	@PostMapping("/users/login")
+	public boolean loginUser(@RequestBody UserM userM) {
+		return User.login(userM.username(), userM.password());
+	}
+	
+	
+	
+	// provjeri je li lozinka valjana
+	@PostMapping("/users/{username}/check-password")
+	public boolean checkPassword(@PathVariable String username, @RequestBody String passwordAttempt) {
+		return Encryption.testPassword(passwordAttempt, User.getEPassword(username));
+	}
+	
+	
+	
+	// uplata/isplata sa racuna
+	@PostMapping("/users/{username}/make-payment")
+	public Integer payment(@PathVariable String username, @RequestBody double payment) {
+		return User.makePayment(username, payment);
+	}
+	
+	// obavljanje transakcije
+	@PostMapping("/users/{username}/make-transaction")
+	public Integer transaction(@PathVariable String username, @RequestBody TransactionReq transactionReq) {
+		return User.makeTransaction(username, transactionReq.recipient(), transactionReq.amount());
+	}
+	
+	
+	
+	// brisanje korisnikoovih datoteka
+	@DeleteMapping("/users/{username}")
+	public void deleteUser(@PathVariable String username) {
+		if (User.deleteFiles(username) == false)
+			System.out.println(" !!! DATOTEKE KORISNIKA " + username + " NISU USPJESNO OBRISANE !!! ");
+	}	
+}
